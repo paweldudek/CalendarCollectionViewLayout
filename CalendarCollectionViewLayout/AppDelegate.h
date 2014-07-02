e4be297fae971649bc1cd6e7ab51399234fcdfdb
Pawel Dudek <pawel@dudek.mobi>
2014-07-02 16:56:10 +0200

    Initial commit

---
+++ CalendarCollectionViewLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalendarCollectionViewLayout
+//
+//  Created by Pawel Dudek on 02/07/14.
+//  Copyright (c) 2014 dudek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalendarCollectionViewLayoutTests/CalendarCollectionViewLayoutTests.m
@@ -0,0 +1,34 @@
+//
+//  CalendarCollectionViewLayoutTests.m
+//  CalendarCollectionViewLayoutTests
+//
+//  Created by Pawel Dudek on 02/07/14.
+//  Copyright (c) 2014 dudek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CalendarCollectionViewLayoutTests : XCTestCase
+
+@end
+
+@implementation CalendarCollectionViewLayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
