9e1cebadb27d20f29702574cc6ba86fd2cd6b2bb
Paweł Dudek <pawel@dudek.mobi>
2016-01-30 17:11:09 +0100

    Fixed issues with crashing upon layout attributes change

--- CalendarCollectionViewLayout/Layout/Attributes/CalendarLayoutAttributes.m
+++ CalendarCollectionViewLayout/Layout/Attributes/CalendarLayoutAttributes.m
@@ -18,7 +18,16 @@
 - (BOOL)isEqual:(id)other {
     BOOL equal = [super isEqual:other];
     if (equal) {
-        equal &= [self.separatorColor isEqual:[other separatorColor]];
+        if (self.separatorColor && [other separatorColor]) {
+            equal &= [self.separatorColor isEqual:[other separatorColor]];
+        }
+        else if (self.separatorColor || [other separatorColor]) {
+            equal &= NO;
+        }
+        else {
+            equal &= YES;
+        }
+        
 
         if (self.separatorText && [other separatorText]) {
             equal &= [self.separatorText isEqual:[other separatorText]];
